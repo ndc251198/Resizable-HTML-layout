04c73aff43f3fd3e8ed3492a38daae24cc9aa0b4
ndc251198 <ndc251198@gmail.com>
2019-06-16 10:42:56 +0700

    fix error

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,44 +1,43 @@
-function resizeElementsWithResizeBar (resizeBar, firstElement, secondElement, dimention = "vertical") {
-      var dragBar = $(resizeBar);
-      var firstElement = $(firstElement);
-      var secondElement = $(secondElement);
-      var containerElement = firstElement.parent();
-      var firstPercentage;
-      var dimCSS;
-      var isMouseDown = false;
+function resizeElementsWithResizeBar(resizeBar, firstElement, secondElement, dimention = "vertical") {
+    var dragBar = $(resizeBar);
+    var firstElement = $(firstElement);
+    var secondElement = $(secondElement);
+    var containerElement = firstElement.parent();
+    var firstPercentage;
+    var dimCSS;
+    var isMouseDown = false;
 
-      if (dimention == "vertical") {
+    if (dimention == "vertical") {
         dimCSS = 'height';
         dragBar.css('cursor', 'row-resize');
-      }
-      else {
+    } else {
         dimCSS = 'width';
         dragBar.css('cursor', 'col-resize');
-      }
+    }
 
-      $(document).on('mousedown', resizeBar, function() {
+    $(document).on('mousedown', resizeBar, function() {
         isMouseDown = true;
-      })
-      $(document).on('mouseup', function() {
+    })
+    $(document).on('mouseup', function() {
         isMouseDown = false;
-      })
-      $(document).on('mousemove', function(event) {
-        event.preventDefault();
+    })
+    $(document).on('mousemove', function(event) {
+        event.stopPropagation();
         if (isMouseDown) {
-          var offset = containerElement.offset();
-          if (dimention == "vertical") {
-            firstPercentage = ((event.pageY - offset.top) / heightToNumber(containerElement.css('height'))) * 100;
-          }
-          else {
-            firstPercentage = ((event.pageX - offset.left) / heightToNumber(containerElement.css('width'))) * 100;
-          }
-          
-          firstElement.css(dimCSS, firstPercentage + '%');
-          secondElement.css(dimCSS, (100 - firstPercentage) + "%");
-        }      
-      })
-      function heightToNumber(height) {
+            var offset = containerElement.offset();
+            if (dimention == "vertical") {
+                firstPercentage = ((event.pageY - offset.top) / heightToNumber(containerElement.css('height'))) * 100;
+            } else {
+                firstPercentage = ((event.pageX - offset.left) / heightToNumber(containerElement.css('width'))) * 100;
+            }
+
+            firstElement.css(dimCSS, firstPercentage + '%');
+            secondElement.css(dimCSS, (100 - firstPercentage) + "%");
+        }
+    })
+
+    function heightToNumber(height) {
         height = height.replace("px", "");
         return +height;
-      }
     }
+}
